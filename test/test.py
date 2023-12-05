1de829ed11c50ddb3bfd45ecf8f2bf22c61302b5
Turrrdidae <1945257314@qq.com>
2023-12-05 15:16:25 +0800

    修改排序函数所属文件名，避免调用出错；添加了test代码

--- test/test.py
+++ test/test.py
@@ -1,1 +1,17 @@
+import random
+import sys
+sys.path.append('src/')
+from Insertion_Sort import *
 
+def main():
+    len = 20
+    arr = [random.randint(1, 100) for _ in range(len)]
+
+    print("原始数组：", arr)
+
+    insertion_sort(arr)
+
+    print("排序后的数组：", arr)
+
+if __name__ == "__main__":
+    main()
