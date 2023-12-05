d637ea0788c4360c8731de8e4d252885f1d57bc0
Turrrdidae <112929926+Turrrdidae@users.noreply.github.com>
2023-12-05 16:44:20 +0800

    Update Insertion_Sort.py

--- src/Insertion_Sort.py
+++ src/Insertion_Sort.py
@@ -4,6 +4,7 @@ def insertion_sort(arr):
         #循环数组
         v = arr[i]
         j = i - 1
+        # 遍历找到合适的插入位置
         while j >= 0 and arr[j] > v:
             arr[j + 1] = arr[j]
             j -= 1
